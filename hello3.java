d641f3982e01f7675f6a537d0225a232a83d83e5
unknown <juliusahianu@gmail.com>
2019-06-17 10:30:31 -0700

    added hello3

---
+++ hello3.java
@@ -0,0 +1,12 @@
+class hello3{
+
+    public static void main(String[] args){
+
+    	String foo="IAP 6.092";
+    	System.out.println(foo);
+    	foo="something else";
+    	System.out.println(foo);
+    }
+
+
+}
